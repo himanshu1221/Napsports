1e58d1f6b020c0429cb9ef62806060d3a4b6db50
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-01-17 17:16:23 +0530

    removed slider

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,21 +1,3 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-
-import "./home.css";
-
-// import required modules
-import { Autoplay, Pagination, Navigation } from "swiper";
-
-import Store1 from '../../images/store1.jpg'
-import Store2 from '../../images/store2.jpg'
-import Store4 from '../../images/store4.jpeg'
-import Store5 from '../../images/store5.jpg'
 import Navbar from '../Layout/Navbar';
 import Footer from '../Layout/Footer';
 
@@ -23,7 +5,7 @@ export default function Home() {
   return (
     <>
     <Navbar/>
-      <Footer/>
+    <Footer/>
     </>
   );
 }
