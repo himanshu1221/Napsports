d830633674ad15f5b8384b91a45cf45cfc381300
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-01-18 17:19:51 +0530

    added background

--- src/Components/home_content/Content.js
+++ src/Components/home_content/Content.js
@@ -4,9 +4,24 @@ import './content.css'
 const Content = () => {
   return (
     <>
-    <div className="rock">
-    <img src={Rock} alt="Rock"/>
-    </div>
+
+<div class="container-fluid main_header ">
+  <div class="row">
+      <div class="col-md-10 col-12 mx-auto">
+          <div class="row">
+            <div class="col-md-6 col-12 main_header_right">
+                <figure>
+                    <img src={Rock} class="img-fluid"  alt=""/>
+                </figure>
+            </div>
+            <div class="col-md-6 col-12 main_header_left">
+              <p>Welcome To Nap Store !</p>
+              <h1>  Your One stop Solution For All your <span>Sports Wear</span> And All The <span>Sports Accessories</span></h1>
+            </div>
+          </div>
+      </div>
+  </div>
+</div>
     </>
   )
 }
