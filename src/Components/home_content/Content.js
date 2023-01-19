9781f8e7b5f2792cfdbaff620cde077b51adf8d4
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-01-19 12:41:56 +0530

    removed bootstrap

--- src/Components/home_content/Content.js
+++ src/Components/home_content/Content.js
@@ -5,22 +5,16 @@ const Content = () => {
   return (
     <>
 
-<div class="container-fluid main_header ">
-  <div class="row">
-      <div class="col-md-10 col-12 mx-auto">
-          <div class="row">
-            <div class="col-md-6 col-12 main_header_right">
+<div class="main_header ">
+            <div class="main_header_right">
                 <figure>
                     <img src={Rock} class="img-fluid"  alt=""/>
                 </figure>
             </div>
-            <div class="col-md-6 col-12 main_header_left">
-              <p>Welcome To Nap Store !</p>
+            <div class="main_header_left">
+              <p>Welcome To <span> Nap Store </span> !</p>
               <h1>  Your One stop Solution For All your <span>Sports Wear</span> And All The <span>Sports Accessories</span></h1>
             </div>
-          </div>
-      </div>
-  </div>
 </div>
     </>
   )
