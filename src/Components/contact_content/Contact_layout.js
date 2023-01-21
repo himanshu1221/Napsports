67255116a8ed43a7ae3c39cda0510497051f6ccb
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-01-21 16:11:10 +0530

    added contact us form

--- src/Components/contact_content/Contact_layout.js
+++ src/Components/contact_content/Contact_layout.js
@@ -1,23 +1,62 @@
 import React from 'react'
 import './contact_layout.css'
-import { MapContainer, TileLayer, Marker,Popup} from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Contact_layout = () => {
   return (
     <>
-  <div className="card">
-    <h1>Talk with our sales team</h1>
-      <div className="map-wrap">
-        <MapContainer center={[26.2008,78.17435]} zoom={12}>
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <Marker position={[26.2008,78.17435]}>
-          <Popup>Location Of Deen Dayal Mall (Gwalior) </Popup>
-          </Marker>
-        </MapContainer>
+      <h1 className='mt-10 mb-20'>Talk with our sales team</h1>
+      <div className="min-h-screen py-26 ">
+        <div className="container mx-auto">
+          <div className="flex flex-col lg:flex-row w-8/12 mx-auto bg-black text-white rounded-xl shadow-lg overflow-hidden" >
+            <div className='w-full lg:w-1/2 bg-black flex flex-col items-center justify-center'>
+              <h1 className='mb-14'>Location</h1>
+              <div>
+                <MapContainer center={[26.2008, 78.17435]} zoom={12}>
+                  <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                  <Marker position={[26.2008, 78.17435]}>
+                    <Popup>Location Of Deen Dayal Mall (Gwalior) </Popup>
+                  </Marker>
+                </MapContainer>
+              </div>
+            </div>
+            <div className='w-full lg:w-1/2 py-16 px-12 form '>
+              <h1 className='mb-4'>Query Form</h1>
+              <form action="#">
+                <div className='grid grid-cols-2 gap-5'>
+                      <input className='border border-gray-400 py-1 px-2 mt-10' type="text" id="fname" name="firstname" placeholder="Your First Name.."></input>
+                      <input className='border border-gray-400 py-1 px-2 mt-10' type="text" id="lname" name='lastname' placeholder='your Last Name' />
+                </div>
+                <div className="mt-10">
+                <input className='border border-gray-400 py-1 px-2 w-full' type="email" id="email" name='email' placeholder='your@xyz' />
+                </div>
+                <div className="mt-10">
+                <input className='border border-gray-400 py-1 px-2 w-full' type="number"   placeholder='Contact Number' />
+                </div>
+                <div className="mt-12 mb-10">
+                  <button className='w-full bg-purple-500 py-3 text-center text-white'>Submit</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-  </div>
     </>
   )
 }
 
 export default Contact_layout
+{/* <div className="card">
+<div className='form'>
+<form action="">
+  <label htmlFor="fname">First Name</label>
+  <input type="text" id="fname" name="firstname" placeholder="Your First Name.."></input>
+  <label htmlFor="lname">Last Name</label>
+  <input type="text" id="lname" name='lastname' placeholder='your Last Name' />
+  <label htmlFor="email">Your Email</label>
+  <input type="email" id='email' name='email' placeholder='Your@xyz' />
+  <label htmlFor="subject">Subject</label>
+  <textarea name="subject" id="subject" placeholder='Your Query' cols="20" rows="5"></textarea>
+</form>
+</div>
+</div> */}
