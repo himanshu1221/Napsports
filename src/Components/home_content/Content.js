fccec2aef95525291bcd5c150d6d410160b9e487
OkenHaha <keithellakpamoken@gmail.com>
2023-01-25 21:23:17 +0530

    made some changes to the content comonent

--- src/Components/home_content/Content.js
+++ src/Components/home_content/Content.js
@@ -6,14 +6,14 @@ const Content = () => {
     <>
 
 <div class="main_header ">
+            <div class="main_header_left">
+              <p>Welcome To <span> Nap Store </span> !</p>
+              <h1>  Your One stop Solution For All your <span>Sports Wear</span> And All The <span>Sports Accessories</span></h1>
+            </div>
             <div class="main_header_right">
                 <figure>
                     <img src={Rock} class="img-fluid"  alt=""/>
                 </figure>
-            </div>
-            <div class="main_header_left">
-              <p>Welcome To <span> Nap Store </span> !</p>
-              <h1>  Your One stop Solution For All your <span>Sports Wear</span> And All The <span>Sports Accessories</span></h1>
             </div>
 </div>
     </>
