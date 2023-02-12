4db444b9a64380004d13464b9b25d31b2f4ffb89
OkenHaha <keithellakpamoken@gmail.com>
2023-02-12 22:56:15 +0530

    fixed the responsiveness

--- src/Components/home_content/Content.js
+++ src/Components/home_content/Content.js
@@ -5,16 +5,18 @@ const Content = () => {
   return (
     <>
 
-<div class="main_header ">
-            <div class="main_header_left">
+<div className="main_header container mx-auto p-10">
+          <div className="flex flex-row flex-wrap">
+            <div className="main_header_left w-full md:w-1/2 p-10">
               <p>Welcome To <span class="store_main"> Nap Store</span></p>
               <h1>  Your One stop Solution For All your <span>Sports Wear</span> & <span>Sports Accessories</span></h1>
             </div>
-            <div class="main_header_right">
+            <div class="main_header_right w-full md:w-1/2 p-10">
                 <figure>
                     <img src={Rock} class="img-fluid"  alt=""/>
                 </figure>
             </div>
+          </div>
 </div>
     </>
   )
